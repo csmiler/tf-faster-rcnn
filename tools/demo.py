99d402bbf7f87713d114f8b6016700c29b06eb83
WANG ZHOU Wang.Zhou@ibm.com <wangzhou@dccxl001.pok.ibm.com>
2018-08-03 16:57:14 -0400

    voc2007 20cls baseline

--- tools/demo.py
+++ tools/demo.py
@@ -37,7 +37,7 @@ CLASSES = ('__background__',
            'motorbike', 'person', 'pottedplant',
            'sheep', 'sofa', 'train', 'tvmonitor')
 
-NETS = {'vgg16': ('vgg16_faster_rcnn_iter_70000.ckpt',),'res101': ('res101_faster_rcnn_iter_110000.ckpt',)}
+NETS = {'vgg16': ('vgg16_faster_rcnn_iter_70000.ckpt',),'res101': ('res101_faster_rcnn_iter_70000.ckpt',)}
 DATASETS= {'pascal_voc': ('voc_2007_trainval',),'pascal_voc_0712': ('voc_2007_trainval+voc_2012_trainval',)}
 
 def vis_detections(im, class_name, dets, thresh=0.5):
